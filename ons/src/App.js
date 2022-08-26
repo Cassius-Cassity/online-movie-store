488410923807499eb5b4bf261d8095ab34c7d741
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2022-08-26 17:25:36 -0600

    refactored to add search bar functionality, user can now search for a movie

--- ons/src/App.js
+++ ons/src/App.js
@@ -1,30 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import React from 'react';
 import Header from './components/Header';
 import axios from 'axios';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const key = process.env.REACT_APP_FEATURE_KEY;
-
-  useEffect(() => {
-    axios.get(key)
-    .then((res) => {
-      setMovies(res.data.results);
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  }, []);
-
   
   return (
     <div className="movie-container">
       <Header/>
-         {movies.length > 0 && movies.map((movie) =>
-          <Movie key={movie.id} {...movie} />
-         )}
     </div>
   );
 }
--- ons/src/components/Header.js
+++ ons/src/components/Header.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Movie from './Movie';
+import axios from 'axios';
 
 
 function Header(){
     const [searchTerm, setSearchTerm ] = useState("")
+    const [movies, setMovies] = useState([]);
+
+    const searchKey = process.env.REACT_APP_SEARCH_KEY;
+    const key = process.env.REACT_APP_FEATURE_KEY;
+
+    useEffect(() => {
+        axios.get(key)
+        .then((res) => {
+          setMovies(res.data.results);
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }, []);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
+        axios.get(searchKey + searchTerm)
+        .then((res) => {
+            console.log(res.data.results)
+            setMovies(res.data.results)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     };
 
     const handleChange = (event) => {
@@ -26,6 +49,9 @@ function Header(){
                     />
                 </form>
             </div>
+            {movies.length > 0 && movies.map((movie) =>
+                <Movie key={movie.id} {...movie} />
+            )}
         </div>
     )
 };
